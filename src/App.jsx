34a4c01ad76fbd8ce01009016fc22899132acd36
Naushad Lucky <32879670+nshd0@users.noreply.github.com>
2026-07-03 09:57:04 +0530

    Update App.jsx routing with all 7 pages: Home, Dashboards, Curriculum, QuestionBank, Reports, Settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import TopBar from './components/TopBar'
+import Home from './pages/Home'
 import TeacherDashboard from './pages/TeacherDashboard'
 import StudentDashboard from './pages/StudentDashboard'
-import TopBar from './components/TopBar'
+import Curriculum from './pages/Curriculum'
+import QuestionBank from './pages/QuestionBank'
+import Reports from './pages/Reports'
+import Settings from './pages/Settings'
 
 function App() {
   return (
     <Router>
       <TopBar />
       <Routes>
-        <Route path="/" element={<Navigate to="/teacher" replace />} />
+        <Route path="/" element={<Home />} />
         <Route path="/teacher" element={<TeacherDashboard />} />
         <Route path="/student" element={<StudentDashboard />} />
+        <Route path="/curriculum" element={<Curriculum />} />
+        <Route path="/question-bank" element={<QuestionBank />} />
+        <Route path="/reports" element={<Reports />} />
+        <Route path="/settings" element={<Settings />} />
       </Routes>
     </Router>
   )
